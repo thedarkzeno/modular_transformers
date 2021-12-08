dbf368db44e0cd467ae9b95cc8b2dedd73567888
thedarkzeno <multiolx@gmail.com>
2021-12-08 19:55:57 -0300

    add evaluation step

--- mlm_fly.py
+++ mlm_fly.py
@@ -6,18 +6,21 @@ from torch.utils.data.dataloader import DataLoader
 from tqdm import tqdm
 from transformers import get_linear_schedule_with_warmup
 import torch.nn as nn
+import math
+
 
 def getIgnoreMask(tokens_tensor, ignore_tokens):
-  result = []
-  finished = False
-  for v in tokens_tensor:
-    if finished == False:
-      result.append(v in ignore_tokens)
-      if v == 0:         ### Once it gets to the padding you don't need to check anymore
-        finished = True
-    else:
-      result.append(True)
-  return result
+    result = []
+    finished = False
+    for v in tokens_tensor:
+        if finished == False:
+            result.append(v in ignore_tokens)
+            if v == 0:  # Once it gets to the padding you don't need to check anymore
+                finished = True
+        else:
+            result.append(True)
+    return result
+
 
 def torch_mask_tokens(inputs, tokenizer, mlm_probability=0.15):
     """
@@ -26,25 +29,27 @@ def torch_mask_tokens(inputs, tokenizer, mlm_probability=0.15):
     labels = inputs.clone()
     # We sample a few tokens in each sequence for MLM training (with probability `self.mlm_probability`)
     probability_matrix = torch.full(labels.shape, mlm_probability)
-    
+
     special_tokens_mask = [
-                tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()
-            ]
+        tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()
+    ]
     special_tokens_mask = torch.tensor(special_tokens_mask, dtype=torch.bool)
-    
 
     probability_matrix.masked_fill_(special_tokens_mask, value=0.0)
     masked_indices = torch.bernoulli(probability_matrix).bool()
     labels[~masked_indices] = -100  # We only compute loss on masked tokens
-    
 
     # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
-    indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
-    inputs[indices_replaced] = 103 #self.tokenizer.convert_tokens_to_ids(self.tokenizer.mask_token)
+    indices_replaced = torch.bernoulli(torch.full(
+        labels.shape, 0.8)).bool() & masked_indices
+    # self.tokenizer.convert_tokens_to_ids(self.tokenizer.mask_token)
+    inputs[indices_replaced] = 103
 
     # 10% of the time, we replace masked input tokens with random word
-    indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
-    random_words = torch.randint(len(tokenizer), labels.shape, dtype=torch.long)
+    indices_random = torch.bernoulli(torch.full(
+        labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+    random_words = torch.randint(
+        len(tokenizer), labels.shape, dtype=torch.long)
     inputs[indices_random] = random_words[indices_random]
 
     # The rest of the time (10% of the time) we keep the masked input tokens unchanged
@@ -52,57 +57,94 @@ def torch_mask_tokens(inputs, tokenizer, mlm_probability=0.15):
 
 
 class Trainer():
-  def __init__(self, tokenizer):
-    self.tokenizer=tokenizer
-    self.train_data = None
-    self.train_dataloader = None
-  
-  def process_data_to_model_inputs(self, batch):
-    tokens = torch.tensor(self.tokenizer.batch_encode_plus(batch["text"], padding="max_length", truncation=True, max_length=128)["input_ids"])
-    inputs, labels, mask = torch_mask_tokens(tokens, tokenizer=self.tokenizer)
-    batch["input"] = inputs
-    batch["mask"] = mask
-    batch["labels"] = labels
-    return batch
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+        self.train_data = None
+        self.train_dataloader = None
+        self.eval_data = None
+        self.eval_dataloader = None
 
-  def prepare_dataset(self, file, batch_size):
-    self.train_data = load_dataset('text', data_files={'train': file})
-    self.train_dataloader = DataLoader(
-        self.train_data["train"], shuffle=True, batch_size=batch_size
-    )
-    
-  def fit_mlm(self, model, epochs=1, learning_rate=1e-4, warmup_steps=1000, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.01, device="cpu"):
-    assert (self.train_dataloader is not None), "You need to prepare the dataset first"
-    criterion = nn.CrossEntropyLoss(ignore_index=-100)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, betas=betas, eps=eps, weight_decay=weight_decay, amsgrad=False)
-    steps = self.train_dataloader.dataset.num_rows//self.train_dataloader.batch_size 
-    scheduler = get_linear_schedule_with_warmup(optimizer, warmup_steps, steps)
-    model.to(device)
+    def process_data_to_model_inputs(self, batch):
+        tokens = torch.tensor(self.tokenizer.batch_encode_plus(
+            batch["text"], padding="max_length", truncation=True, max_length=128)["input_ids"])
+        inputs, labels, mask = torch_mask_tokens(
+            tokens, tokenizer=self.tokenizer)
+        batch["input"] = inputs
+        batch["mask"] = mask
+        batch["labels"] = labels
+        return batch
 
-    #training loop
-    pbar = tqdm(range(epochs), desc="Epochs")
-    for epoch in pbar:
-      model.train()
-      for step, batch in enumerate(self.train_dataloader):
-        optimizer.zero_grad()
-        batch = self.process_data_to_model_inputs(batch)
-        # batch.set_format(type='torch', columns=['input', 'labels', 'mask'])
-        outputs = model(batch['input'].to(device))#model(batch['input'].to(device), batch['mask'].to(device))
-        label = batch['labels'].to(device)
+    def prepare_dataset(self, train_file, batch_size, val_file=None):
+        self.train_data = load_dataset('text', data_files={'train': train_file})
+        self.train_dataloader = DataLoader(
+            self.train_data["train"], shuffle=True, batch_size=batch_size
+        )
+        if val_file is not None:
+          self.eval_data = load_dataset("text", data_files={'val': val_file})
+          self.eval_dataloader = DataLoader(
+            self.eval_data["val"], shuffle=True, batch_size=batch_size
+          )
 
-        loss = criterion(outputs.transpose(1, 2), label)
+    def fit_mlm(self, model, epochs=1, learning_rate=1e-4, warmup_steps=1000, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.01, eval_steps=1000, device="cpu"):
+        assert (
+            self.train_dataloader is not None), "You need to prepare the dataset first"
+        criterion = nn.CrossEntropyLoss(ignore_index=-100)
+        optimizer = torch.optim.AdamW(model.parameters(
+        ), lr=learning_rate, betas=betas, eps=eps, weight_decay=weight_decay, amsgrad=False)
+        steps = self.train_dataloader.dataset.num_rows//self.train_dataloader.batch_size
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer, warmup_steps, steps)
+        model.to(device)
 
-        # accuracy = jnp.equal(jnp.argmax(logits, axis=-1), label) * label_mask
+        # training loop
+        pbar = tqdm(range(epochs), desc="Epochs")
+        for epoch in pbar:
 
-        loss.backward()
-        optimizer.step()
-        scheduler.step()
-        log_ = {
-            'Train Loss': loss,
-            'Step': step,
-            'LR': scheduler.get_last_lr()[0]
-        }
-        pbar.set_postfix(log_)
+            for step, batch in enumerate(self.train_dataloader):
+                model.train()
+                optimizer.zero_grad()
+                batch = self.process_data_to_model_inputs(batch)
+                # batch.set_format(type='torch', columns=['input', 'labels', 'mask'])
+                # model(batch['input'].to(device), batch['mask'].to(device))
+                outputs = model(batch['input'].to(device))
+                label = batch['labels'].to(device)
 
+                loss = criterion(outputs.transpose(1, 2), label)
 
-        
+                # accuracy = jnp.equal(jnp.argmax(logits, axis=-1), label) * label_mask
+
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
+                # eval step
+                perplexity = 0
+                if self.eval_dataloader is not None and (step+1) % eval_steps == 0:
+                    model.eval()
+                    losses = []
+                    for eval_step, eval_batch in enumerate(self.eval_dataloader):
+                        with torch.no_grad():
+                            outputs = model(**eval_batch)
+
+                        loss = outputs.loss
+                        losses.append(loss)
+
+                    losses = torch.cat(losses)
+                    losses = losses[: len(self.eval_data)]
+                    try:
+                        perplexity = math.exp(torch.mean(losses))
+                    except OverflowError:
+                        perplexity = float("inf")
+                if perplexity != 0:
+                    log_ = {
+                        'Train Loss': loss,
+                        'Step': step,
+                        'LR': scheduler.get_last_lr()[0],
+                        'perplexity': perplexity
+                    }
+                else:
+                    log_ = {
+                        'Train Loss': loss,
+                        'Step': step,
+                        'LR': scheduler.get_last_lr()[0]
+                    }
+                pbar.set_postfix(log_)
