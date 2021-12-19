7c3df5cdc7edff673e7de0715d219ca88e20e648
thedarkzeno <multiolx@gmail.com>
2021-12-19 00:58:37 -0300

    change BertPreTrainedModel config

--- model/encoder.py
+++ model/encoder.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+from .config import Config
 from transformers.models.bert.modeling_bert import *
 from transformers.modeling_utils import (
     PreTrainedModel,
@@ -277,37 +278,37 @@ class ModelEncoder(nn.Module):
             cross_attentions=all_cross_attentions,
         )
 
-# class ModularPreTrainedModel(PreTrainedModel):
-#     """
-#     An abstract class to handle weights initialization and a simple interface for downloading and loading pretrained
-#     models.
-#     """
+class BertPreTrainedModel(PreTrainedModel):
+    """
+    An abstract class to handle weights initialization and a simple interface for downloading and loading pretrained
+    models.
+    """
 
-#     config_class = Config
-#     load_tf_weights = load_tf_weights_in_bert
-#     base_model_prefix = "modular"
-#     supports_gradient_checkpointing = True
-#     _keys_to_ignore_on_load_missing = [r"position_ids"]
+    config_class = Config
+    load_tf_weights = load_tf_weights_in_bert
+    base_model_prefix = "bert"
+    supports_gradient_checkpointing = True
+    _keys_to_ignore_on_load_missing = [r"position_ids"]
 
-#     def _init_weights(self, module):
-#         """Initialize the weights"""
-#         if isinstance(module, nn.Linear):
-#             # Slightly different from the TF version which uses truncated_normal for initialization
-#             # cf https://github.com/pytorch/pytorch/pull/5617
-#             module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-#             if module.bias is not None:
-#                 module.bias.data.zero_()
-#         elif isinstance(module, nn.Embedding):
-#             module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-#             if module.padding_idx is not None:
-#                 module.weight.data[module.padding_idx].zero_()
-#         elif isinstance(module, nn.LayerNorm):
-#             module.bias.data.zero_()
-#             module.weight.data.fill_(1.0)
+    def _init_weights(self, module):
+        """Initialize the weights"""
+        if isinstance(module, nn.Linear):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            # cf https://github.com/pytorch/pytorch/pull/5617
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, nn.Embedding):
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if module.padding_idx is not None:
+                module.weight.data[module.padding_idx].zero_()
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
 
-#     def _set_gradient_checkpointing(self, module, value=False):
-#         if isinstance(module, ModelEncoder):
-#             module.gradient_checkpointing = value
+    def _set_gradient_checkpointing(self, module, value=False):
+        if isinstance(module, ModelEncoder):
+            module.gradient_checkpointing = value
 
 
 class Model(BertPreTrainedModel):
