5801182b3caf5fe2244e58795e6fb1a86060fd7c
thedarkzeno <multiolx@gmail.com>
2021-12-24 10:18:23 -0300

    flax and torch check

--- model/attention/__init__.py
+++ model/attention/__init__.py
@@ -1,3 +1,6 @@
-from .fourier import fourier_transform
-from .fourier_flax import Fourier_transform_flax
-from .self_attention import SelfAttention, AttentionHead
+from ..file_utils import is_torch_available, is_flax_available
+if is_torch_available():
+    from .fourier import fourier_transform
+    from .self_attention import SelfAttention, AttentionHead
+if is_flax_available():
+    from .fourier_flax import Fourier_transform_flax
